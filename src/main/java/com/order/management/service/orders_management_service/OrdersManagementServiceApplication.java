594fb04eb4303b1521a99179ba9b26cd97305490
Kotturi858 <kotturi858bharath@gmail.com>
2025-04-08 19:39:07 +0530

    initial commit

---
+++ src/main/java/com/order/management/service/orders_management_service/OrdersManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.order.management.service.orders_management_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdersManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdersManagementServiceApplication.class, args);
+	}
+
+}
